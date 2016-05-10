a53064ac2bdc3feb27d1c40cad7c57fc2f4ded0e
mnorelli <mnorelli360@gmail.com>
2016-05-09 19:35:55 -0700

    add myReduce, passing 8 of 14 tests

--- myReduce.js
+++ myReduce.js
@@ -4,6 +4,14 @@
 function myReduce(arr, callback) {
 
 //  CODE INSIDE HERE   //
+  var previousValue;
+  for (i=1;i<arr.length;i+=1){
+    if(previousValue == null) var previousValue = arr[i-1];
+    var currentValue = arr[i];
+    previousValue = callback(previousValue,currentValue)
+    }
+
+  return previousValue;
 
 }
 
@@ -20,3 +28,23 @@ function myReduce(arr, callback) {
 
 // export this function (you can ignore this for now)
 module.exports = myReduce;
+
+
+/*
+callback
+Function to execute on each value in the array, taking four arguments:
+previousValue
+The value previously returned in the last invocation of the callback, or initialValue, if supplied. (See below.)
+currentValue
+The current element being processed in the array.
+currentIndex
+The index of the current element being processed in the array.
+array
+The array reduce was called upon.
+The first time the callback is called, 
+previousValue and currentValue can be one of two values. If initialValue is 
+provided in the call to reduce, then previousValue will be equal to 
+initialValue and currentValue will be equal to the first value in the array. 
+If no initialValue was provided, then previousValue will be equal to the first 
+value in the array and currentValue will be equal to the second.
+*/
